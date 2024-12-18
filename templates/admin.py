749c2a370187e22d697ea8afdedab3528440c283
Sviat-png <prokopenko16032004@gmail.com>
2024-12-18 16:45:45 +0200

    Create admin.py

---
+++ templates/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Locomotive, Repair, `, Worker
+
+admin.site.register(Locomotive)
+admin.site.register(Repair)
+admin.site.register(Brigade)
+admin.site.register(Worker)
